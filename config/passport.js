a2612305c8379bed76e423aec2dcd2a75a328b79
JingYi120 <l2898168@yahoo.com.tw>
2023-02-18 03:36:09 +0800

    feat: add user login feature via passport

---
+++ config/passport.js
@@ -0,0 +1,77 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const FacebookStrategy = require('passport-facebook').Strategy
+
+module.exports = app => {
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // 登入的驗證機制
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
+    User.findOne({ email })
+      .then(user => {
+        // 使用者不存在
+        if (!user) {
+          return done(null, false, req.flash('warning_msg', 'That email is not registered!'))
+        }
+
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, req.flash('warning_msg', 'Email or Password incorrect.'))
+            }
+            // 登入成功
+            return done(null, user)
+          })
+      })
+      .catch(err => done(err, false))
+  }))
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    // console.log(profile)
+    const { name, email } = profile._json
+
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+
+        // 刻意設定一串亂碼，
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt =>
+            bcrypt.hash(randomPassword, salt)
+          )
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+
+      })
+  }))
+
+  // 序列化
+  passport.serializeUser((user, done) => {
+    // console.log(user)
+    done(null, user.id)
+  })
+
+  // 反序列化
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
+  })
+
+}
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login',
+  failureMessage: true
+
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
