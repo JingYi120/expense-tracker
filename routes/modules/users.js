07856e44e18d56d9d1f642618ce7a454950971fc
JingYi120 <l2898168@yahoo.com.tw>
2023-02-18 03:48:39 +0800

    feat: add bcryptjs and register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -17,5 +19,57 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.post('/register', (req, res) => {
+
+  const { name, email, password, confirmPassword } = req.body
+
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符!' })
+  }
+
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        errors.push({ message: '這個Email已經註冊過了' })
+        return res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+
+      return bcrypt
+        .genSalt(10)
+        .then(salt =>
+          bcrypt.hash(password, salt)
+        )
+        .then(hash =>
+          User.create({
+            name,
+            email,
+            password: hash
+          }))
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
+})
 
 module.exports = router
