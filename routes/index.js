14d15a8b724106ec4569a7c9f6b95b5d6c16e505
JingYi120 <l2898168@yahoo.com.tw>
2023-02-18 03:23:40 +0800

    feat: add login, resiger page and feature

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 const router = express.Router()
 
 
 
 router.use('/', home)
 router.use('/records', records)
+router.use('/users', users)
 
 
 
