e56a84aad9969f76eae357331391f017aef8a1e0
JingYi120 <l2898168@yahoo.com.tw>
2023-02-18 14:24:03 +0800

    feat: add search router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -33,4 +33,42 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+router.get('/search', (req, res) => {
+  const userId = req.user._id
+  const categoryId = req.query.categoryId
+
+  if (!categoryId) {
+    return res.redirect('/')
+  }
+
+  Category.find({})
+    .lean()
+    .then(catagories => {
+      catagories.forEach(category => {
+        if (String(category._id) === categoryId) {
+          category.preset = true
+        } else {
+          category.preset = false
+        }
+      })
+      Record.find({ userId, categoryId })
+        .populate('categoryId')
+        .lean()
+        .sort({ date: 'desc' })
+        .then(records => {
+          let totalAmount = 0
+          records.forEach(record => {
+            totalAmount += record.amount
+            record.date = moment(record.date).format('YYYY-MM-DD')
+          })
+
+          return res.render('index', {
+            catagories,
+            records,
+            totalAmount
+          })
+        })
+    })
+    .catch(error => console.error(error))
+})
 module.exports = router
